0012a15012ec8b60ea9f9f15fa23339e4d5e9cd4
maksym <marudenko96@gmail.com>
2018-04-01 00:51:26 +0300

    add client build

--- src/main/java/root/appConfiguration/WebConfig.java
+++ src/main/java/root/appConfiguration/WebConfig.java
@@ -4,10 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {

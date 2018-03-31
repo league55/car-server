b748c670fcec839bba62774339baa8caef759550
maksym <marudenko96@gmail.com>
2018-04-01 00:56:34 +0300

    update startup sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,2 @@
 cp opencv_java331.dll /tmp
-cp resources/CarsDrivingUnderBridge.mp4 /tmp
-mvn clean install
-java jar target/cars-monitoring-step2-1.0-SNAPSHOT.jar
+cp src/main/resources/CarsDrivingUnderBridge.mp4 /tmp

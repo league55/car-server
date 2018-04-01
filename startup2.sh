372d2d21765deb15d7bd06164c93cbfa97111434
maksym <marudenko96@gmail.com>
2018-04-01 10:32:36 +0300

    update library source and path to config

--- startup2.sh
+++ startup2.sh
@@ -1,4 +1,2 @@
 echo In startap v2
-cp opencv_java331.dll /tmp
-cp src/main/resources/CarsDrivingUnderBridge.mp4 /tmp
-java -Dserver.port=$PORT $JAVA_OPTS -jar target/*.jar -Djava.library.path="/tmp/"
+cp opencv_java331.dll /tmp; cp src/main/resources/CarsDrivingUnderBridge.mp4 /tmp; java -Dserver.port=$PORT $JAVA_OPTS -jar target/*.jar -Djava.library.path="/tmp/"
